c2c2c2f44988f2a43511964e3f940344090f790b
Tim Broddin <tim.broddin@vrt.be>
2017-03-08 11:47:08 +0100

    random emoji

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const Twit = require('twit');
+const emoji = require('random-emoji');
 const { consumer_key, consumer_secret, access_token, access_token_secret} = process.env;
 const timeout_ms = 60* 1000;
+
+console.log(emoji.random());
+return;
 
 const T = new Twit({ consumer_key, consumer_secret, access_token, access_token_secret, timeout_ms });
 
@@ -12,7 +16,14 @@ stream.on('tweet', function (tweet) {
   if(tweet.user.id_str !== process.env.twitter_id) {
     setTimeout(() => {
       const url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
-      const status = `@mattimeurisse ${url}`;
+      const rnd = emoji.random(5);
+      const length = Math.round(Math.random()*5);
+      let emoji = '';
+      for(let i=0;i<length;i++) {
+        emoji += rnd[i].character;
+      }
+      
+      const status = `@mattimeurisse ${emoji} ${url}`;
 
       T.post('statuses/update', { status }, function(err, data, response) {
         console.log(data)
