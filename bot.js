7dcd2ba81c9cd8def87d8272f62f9faada2c1662
Tim Broddin <tim.broddin@vrt.be>
2017-03-08 13:08:25 +0100

    less spammy

--- bot.js
+++ bot.js
@@ -11,26 +11,41 @@ const T = new Twit({ consumer_key, consumer_secret, access_token, access_token_s
 
 const stream = T.stream('statuses/filter', { track: ['rijst recept', 'rijst recepten', 'rice recipe', 'rice recipes', 'riz recette', 'riz recettes'] })
 
+let queue = [];
+
+setInterval(() => {
+  const randomNumber = Math.round(Math.random()*15);
+  console.log(`Random number = ${randomNumber} - Queue length: ${queue.length}`);
+
+  if(queue.length && randomNumber === 10) {
+    let tweet = queue.shift();
+    const url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
+    const rnd = emoji.random({count: 5});
+    const length = Math.ceil(Math.random()*5);
+    let emojiStr = '';
+    for(let i=0;i<length;i++) {
+      emojiStr += rnd[i].character;
+    }
+
+    const status = `Nieuw recept! ${emojiStr} @mattimeurisse  ${url}`;
+    console.log(status);
+
+    T.post('statuses/update', { status }, function(err, data, response) {
+      //console.log(data)
+      if(err) {
+        console.log(err);
+      }
+    })
+    
+  }
+
+
+}, 60*1000);
+
+
 stream.on('tweet', function (tweet) {
   if(tweet.user.id_str !== process.env.twitter_id) {
-    setTimeout(() => {
-      const url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
-      const rnd = emoji.random({count: 5});
-      const length = Math.ceil(Math.random()*5);
-      let emojiStr = '';
-      for(let i=0;i<length;i++) {
-        emojiStr += rnd[i].character;
-      }
-
-      const status = `Nieuw recept! ${emojiStr} @mattimeurisse  ${url}`;
-      console.log(status);
-
-      T.post('statuses/update', { status }, function(err, data, response) {
-        //console.log(data)
-        if(err) {
-          console.log(err);
-        }
-      })
-    }, 4000);
+    console.log('Push tweet to queue');
+    queue.push(tweet);
   }
 });
